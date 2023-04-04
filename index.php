a5f4ebb39267d1a951a54fb4b338a0a121c3c892
victoriafrench15 <79553936+victoriafrench15@users.noreply.github.com>
2023-04-03 22:04:06 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,21 @@
 <?php
+<!DOCTYPE html>
+<html>
+  <head>
+    <title></title>
+    <meta http-equiv="refresh" content="0; URL='inicio.php'" />
+  </head>
+  <body>
+    <?php
+        require_once "controllers/animales.controller.php";
 
-require_once "controllers/animales.controller.php";
+        require_once "controllers/animales.controller.php";
+                $incio = new ControllerAnimales();
 
-$incio = new ControllerAnimales();
+        $incio = new ControllerAnimales();
+        $incio->ctrGetInicio();
+    ?>
+  </body>
+</html>
 
 $incio->ctrGetInicio();
